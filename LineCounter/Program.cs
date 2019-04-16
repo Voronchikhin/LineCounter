8663b31389f842ddc557125c0740fe7e84059ad2
MVoronchikhin <mvoronchikhin@poidem.ru>
2019-04-16 09:41:08 +0700

    Добавьте файлы проекта.

---
+++ LineCounter/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineCounter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
